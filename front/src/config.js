bf24110721c85ee9ab3cb5a0b68d1a640163fa03
jjeejj <wenjunjiang93@gmail.com>
2025-05-23 10:15:41 +0800

    refactor(config): update contract address and private key env variable

--- front/src/config.js
+++ front/src/config.js
@@ -45,7 +45,7 @@ export const ANVIL_CHAIN = {
 export const CONTRACT_CONFIG = {
     // 本地开发环境
     development: {
-        address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // Hardhat默认部署地址
+        address: "0x9d4454B023096f34B160D6B654540c56A1F81688", // Hardhat默认部署地址
         chainId: 31337, // Hardhat本地网络ID
     },
     // 测试网环境
